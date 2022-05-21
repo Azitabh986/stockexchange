8aa5de84a844bad6a511e19b743ff5f34a1614bc
unknown <azitabhdinesh@gmail.com>
2022-05-21 14:31:45 +0530

    mail is added

--- src/main/java/com/cts/stockexchange/controller/CompanyResource.java
+++ src/main/java/com/cts/stockexchange/controller/CompanyResource.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,6 +28,7 @@ import com.cts.stockexchange.service.CompanyService;
 
 @RestController
 @RequestMapping("/api/v1.0")
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class CompanyResource {
 	
 	private final static Logger logger=org.slf4j.LoggerFactory.getLogger(CompanyResource.class);
